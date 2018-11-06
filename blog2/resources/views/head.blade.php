a0de63864f40e37f0fa9031a7c7f6907f9a36b44
namnd <dangnamk55@gmail.com>
2018-11-06 09:04:52 +0700

    add google anatylics

--- blog2/resources/views/head.blade.php
+++ blog2/resources/views/head.blade.php
@@ -115,6 +115,15 @@
     </div>
 </footer>
 <!-- Footer section end -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-128760109-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-128760109-1');
+</script>
 
 
 
